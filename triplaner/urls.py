658213b182b078b3b47a62f95994c7fdead0f5fc
Oskarbi <oscar.teixe@gmail.com>
2012-10-09 23:06:05 +0200

    Extract the common prefix in urlpatterns

--- triplaner/urls.py
+++ triplaner/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',
-    url(r'^$', 'routeplanner.views.home', name='home'),
-    url(r'^route/$', 'routeplanner.views.get_shortest_path', name='route'),
-    url(r'^connected-stops/$', 'routeplanner.views.get_connected_stops', name='connected-stops'),
+urlpatterns = patterns('routeplanner.views',
+    url(r'^$', 'home', name='home'),
+    url(r'^route/$', 'get_shortest_path', name='route'),
+    url(r'^connected-stops/$', 'get_connected_stops', name='connected-stops'),
 )
